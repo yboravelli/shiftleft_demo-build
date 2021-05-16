d6fca6542dc1f33fa379ef4a914f224b98634f1d
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-05-16 17:54:48 +0100

    Update addPolicies.sh

--- files/addPolicies.sh
+++ files/addPolicies.sh
@@ -18,6 +18,13 @@ curl -k \
   -d '{"name":"evilpetclinic","containers":["*"],"hosts":["*"],"images":["*evilpetclinic*"],"labels":["*"],"appIDs":["*"],"functions":["*"],"namespaces":["*"],"accountIDs":["*"],"codeRepos":["*"],"clusters":["*"],"color":"#68DCFC"}' \
   https://$TL_CONSOLE/api/v1/collections
 
+#add customer malware signature
+curl -k \
+  -u $TL_USER:$TL_PASS \
+  -H 'Content-Type: application/json' \
+  -X PUT \
+  -d '{"feed":[{"name":"evil","md5":"b927cd908b58e703ab86f484b44a3791","allowed":false}]}' 
+  https://$TL_CONSOLE/api/v1/feeds/custom/malware?project=Central+Console
 
 curl -k \
   -u $TL_USER:$TL_PASS \
